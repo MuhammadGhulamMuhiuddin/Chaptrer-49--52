1348f1bf597f0b9f9688e4c895b8ff301a2b6d72
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-11-15 07:48:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+// function displayData() {
+//     var username = document.getElementById("username").value;
+//     var email = document.getElementById("email").value;
+//     var password = document.getElementById("password").value;
+
+//     var resultDiv = document.getElementById("result");
+//     resultDiv.innerHTML = `
+//         <h3>Submitted Data:</h3>
+//         <p><strong>Username:</strong> ${username}</p>
+//         <p><strong>Email:</strong> ${email}</p>
+//         <p><strong>Password:</strong> ${password}</p>
+//     `;
+// }
+
+
+// function toggleText() {
+//     var moreText = document.getElementById("moreText");
+//     var btn = document.getElementById("readMoreBtn");
+
+//     if (moreText.style.display === "none") {
+//         moreText.style.display = "inline";
+//         btn.innerText = "Read less";
+//     } else {
+//         moreText.style.display = "none";
+//         btn.innerText = "Read more";
+//     }
+// }
